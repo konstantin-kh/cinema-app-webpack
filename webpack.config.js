efaea2a9df0fb826c64d0b5e89682522cc5a599d
konstantin-kh <kospomazan@gmail.com>
2019-03-31 23:32:38 +0300

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
 	entry: './src/app.js',
@@ -6,10 +8,13 @@ module.exports = {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
 	},
+	mode: 'development',
+	devtool: 'eval-source-map',
+	watch: true,
 	module: {
 		rules: [{
 			test: /\.m?js$/,
-			exclude: /(node_modules|bower_components)/,
+			exclude: /(node_modules)/,
 			use: {
 				loader: 'babel-loader',
 				options: {
@@ -17,5 +22,17 @@ module.exports = {
 				}
 			}
 		}]
+	},
+	stats: 'minimal',
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './index.html',
+			title: 'Custom template using Handlebars'
+		}),
+		new BundleAnalyzerPlugin()
+	], 
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		port: 9000
 	}
 };
